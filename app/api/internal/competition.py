fca2d48e530a2e66653455d6594458ed9beb019c
laoren2 <102372655+laoren2@users.noreply.github.com>
2025-06-12 06:57:13 +0800

    [feature] support track internal management (#3)

--- app/api/internal/competition.py
+++ app/api/internal/competition.py
@@ -3,13 +3,18 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.session import get_db
 from app.schemas.base import BaseResponse
 from app.schemas.user import AuthContext
-from app.schemas.competition import SeasonCreateForm, EventCreateForm, EventUpdateForm, EventListResponse, RegionCreate
+from app.schemas.competition import (
+    SeasonCreateForm, EventCreateForm, 
+    TrackCreateForm, EventUpdateForm, 
+    TrackUpdateForm, EventListResponse, 
+    TrackListResponse, RegionCreate
+)
 from app.services.competition import (
     create_season_service, 
-    create_event_service, 
-    update_event_service, 
-    update_event_image_url, 
-    query_events_service, 
+    create_event_service, create_track_service,
+    update_event_service, update_track_service,
+    update_event_image_url, update_track_image_url,
+    query_events_service, query_tracks_service,
     create_region_service, 
     update_season_image_url
 )
@@ -89,6 +94,8 @@ async def update_event(
     if event_image:
         event_folder = Path("resources/event") / event.event_id
         event_folder.mkdir(parents=True, exist_ok=True)
+        for file in event_folder.glob("background_*.png"):
+            file.unlink(missing_ok=True)
         bg_path = event_folder / f"background_{int(datetime.now().timestamp())}.png"
         with bg_path.open("wb") as f:
             f.write(await event_image.read())
@@ -120,3 +127,75 @@ async def query_events(
         size=size
     )
     return BaseResponse.success(token=auth.new_token, data=EventListResponse(events=events))
+
+
+# 创建新赛事
+@router.post("/create_track", response_model=BaseResponse[None], summary="创建新赛道")
+async def create_track(
+    track: TrackCreateForm = Depends(),
+    track_image: Optional[UploadFile] = File(None),
+    auth: AuthContext = Depends(get_current_admin),
+    db: AsyncSession = Depends(get_db)
+):
+    image_url = "/resources/placeholder/track.png"
+    new_track = await create_track_service(db, track, image_url)
+
+    if track_image:
+        track_folder = Path("resources/track") / new_track.track_id
+        track_folder.mkdir(parents=True, exist_ok=True)
+        background_path = track_folder / f"background_{int(datetime.now().timestamp())}.png"
+        with background_path.open("wb") as f:
+            f.write(await track_image.read())
+        new_url = f"/resources/track/{new_track.track_id}/{background_path.name}"
+        await update_track_image_url(db, new_track.track_id, new_url)
+
+    return BaseResponse.success(token=auth.new_token, message=f"成功创建赛道:{track.name}", data=None)
+
+
+# 更新赛事
+@router.post("/update_track", response_model=BaseResponse[None], summary="更新赛道")
+async def update_track(
+    track: TrackUpdateForm = Depends(),
+    track_image: Optional[UploadFile] = File(None),
+    auth: AuthContext = Depends(get_current_admin),
+    db: AsyncSession = Depends(get_db)
+):
+    image_url = "/resources/placeholder/track.png"
+    if track_image:
+        track_folder = Path("resources/track") / track.track_id
+        track_folder.mkdir(parents=True, exist_ok=True)
+        for file in track_folder.glob("background_*.png"):
+            file.unlink(missing_ok=True)
+        bg_path = track_folder / f"background_{int(datetime.now().timestamp())}.png"
+        with bg_path.open("wb") as f:
+            f.write(await track_image.read())
+        image_url = f"/resources/track/{track.track_id}/{bg_path.name}"
+    await update_track_service(db, track, image_url)
+
+    return BaseResponse.success(token=auth.new_token, message=f"成功更新赛道:{track.name}", data=None)
+
+
+# 查询赛事
+@router.get("/query_tracks", response_model=BaseResponse[TrackListResponse], summary="查询赛道")
+async def query_tracks(
+    track_name: Optional[str] = Query(None),
+    event_name: Optional[str] = Query(None),
+    season_name: Optional[str] = Query(None),
+    region_name: Optional[str] = Query(None),
+    sport_type: Optional[str] = Query(None),
+    page: int = Query(1, ge=1),
+    size: int = Query(10, ge=1),
+    auth: AuthContext = Depends(get_current_admin),
+    db: AsyncSession = Depends(get_db)
+):
+    tracks = await query_tracks_service(
+        db=db,
+        track_name=track_name,
+        event_name=event_name,
+        season_name=season_name,
+        region_name=region_name,
+        sport_type=sport_type,
+        page=page,
+        size=size
+    )
+    return BaseResponse.success(token=auth.new_token, data=TrackListResponse(tracks=tracks))
--- app/schemas/competition.py
+++ app/schemas/competition.py
@@ -2,7 +2,7 @@ from fastapi import Form
 from app.schemas.base import ORMBase
 from datetime import datetime
 from enum import Enum
-from typing import List
+from typing import List, Optional
 
 
 class SportType(str, Enum):
@@ -26,6 +26,14 @@ class SeasonCreateForm:
         self.start_date = start_date
         self.end_date = end_date
         self.sport_type = sport_type
+
+class SeasonBaseInfo(ORMBase):
+    season_id: str
+    name: str
+    start_date: str
+    end_date: str
+    sport_type: SportType
+    image_url: str
 
 class RegionCreate(ORMBase):
     name: str
@@ -92,10 +100,120 @@ class EventBaseInfo(ORMBase):
 class EventListResponse(ORMBase):
     events: List[EventBaseInfo]
 
-class SeasonBaseInfo(ORMBase):
-    season_id: str
+class TrackCreateForm:
+    name: str
+    start_date: datetime
+    end_date: datetime
+    event_name: str
+    season_name: str
+    region_name: str
+    sport_type: SportType
+    from_latitude: float
+    from_longitude: float
+    to_latitude: float
+    to_longitude: float
+    elevationDifference: int
+    subRegioName: str
+    fee: int
+    prizePool: int
+
+    def __init__(
+        self,
+        name: str = Form(...),
+        start_date: datetime = Form(...),
+        end_date: datetime = Form(...),
+        event_name: str = Form(...),
+        season_name: str = Form(...),
+        region_name: str = Form(...),
+        sport_type: SportType = Form(...),
+        from_latitude: float = Form(...),
+        from_longitude: float = Form(...),
+        to_latitude: float = Form(...),
+        to_longitude: float = Form(...),
+        elevationDifference: int = Form(...),
+        subRegioName: str = Form(...),
+        fee: int = Form(...),
+        prizePool: int = Form(...)
+    ):
+        self.name = name
+        self.start_date = start_date
+        self.end_date = end_date
+        self.event_name = event_name
+        self.season_name = season_name
+        self.region_name = region_name
+        self.sport_type = sport_type
+        self.from_latitude = from_latitude
+        self.from_longitude = from_longitude
+        self.to_latitude = to_latitude
+        self.to_longitude = to_longitude
+        self.elevationDifference = elevationDifference
+        self.subRegioName = subRegioName
+        self.fee = fee
+        self.prizePool = prizePool
+
+
+class TrackUpdateForm:
+    track_id: str
+    name: str
+    start_date: datetime
+    end_date: datetime
+    from_latitude: float
+    from_longitude: float
+    to_latitude: float
+    to_longitude: float
+    elevationDifference: int
+    subRegioName: str
+    fee: int
+    prizePool: int
+
+    def __init__(
+        self,
+        track_id: str = Form(...),
+        name: str = Form(...),
+        start_date: datetime = Form(...),
+        end_date: datetime = Form(...),
+        from_latitude: float = Form(...),
+        from_longitude: float = Form(...),
+        to_latitude: float = Form(...),
+        to_longitude: float = Form(...),
+        elevationDifference: int = Form(...),
+        subRegioName: str = Form(...),
+        fee: int = Form(...),
+        prizePool: int = Form(...)
+    ):
+        self.track_id = track_id
+        self.name = name
+        self.start_date = start_date
+        self.end_date = end_date
+        self.from_latitude = from_latitude
+        self.from_longitude = from_longitude
+        self.to_latitude = to_latitude
+        self.to_longitude = to_longitude
+        self.elevationDifference = elevationDifference
+        self.subRegioName = subRegioName
+        self.fee = fee
+        self.prizePool = prizePool
+
+
+class TrackBaseInfo(ORMBase):
+    track_id: str
     name: str
     start_date: str
     end_date: str
+    event_name: str
+    season_name: str
+    region_name: str
     sport_type: SportType
     image_url: str
+
+    from_latitude: str
+    from_longitude: str
+    to_latitude: str
+    to_longitude: str
+    elevation_difference: str
+    sub_region_name: str
+    fee: str
+    prize_pool: str
+
+class TrackListResponse(ORMBase):
+    tracks: List[TrackBaseInfo]
