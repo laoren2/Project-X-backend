ee1423c1cecd968b8535d98be068d2424cf22ca5
renjie <1397253424@qq.com>
2025-06-12 06:53:05 +0800

    [feature] support track internal management

--- app/crud/competition.py
+++ app/crud/competition.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, update, func
-from app.db.models import Region, Event, Season
+from app.db.models import Region, Event, Season, Track
 from sqlalchemy.orm import selectinload
 from typing import Optional, List
 import uuid
@@ -12,6 +12,7 @@ async def create_season_crud(db: AsyncSession, season: Season) -> Season:
     await db.refresh(season)
     return season
 
+
 async def update_season_crud(db: AsyncSession, season: Season, update_data: dict):
     for field, value in update_data.items():
         setattr(season, field, value)
@@ -19,10 +20,12 @@ async def update_season_crud(db: AsyncSession, season: Season, update_data: dict
     await db.commit()
     await db.refresh(season)
 
+
 async def create_region_crud(db: AsyncSession, region: Region):
     db.add(region)
     await db.commit()
     await db.refresh(region)
+
 
 async def get_region_by_name(db: AsyncSession, name: str) -> Region | None:
     result = await db.execute(select(Region).where(Region.name == name))
@@ -38,12 +41,19 @@ async def get_season_by_name_and_sport_type(db: AsyncSession, name: str, sport_t
     )
     return result.scalar_one_or_none()
 
+
 async def get_season_by_season_id(db: AsyncSession, season_id: str) -> Season | None:
     result = await db.execute(select(Season).where(Season.season_id == season_id))
     return result.scalar_one_or_none()
 
+
 async def get_event_by_event_id(db: AsyncSession, event_id: str) -> Event | None:
     result = await db.execute(select(Event).where(Event.event_id == event_id))
+    return result.scalar_one_or_none()
+
+
+async def get_event_by_name(db: AsyncSession, name: str) -> Event | None:
+    result = await db.execute(select(Event).where(Event.name == name))
     return result.scalar_one_or_none()
 
 
@@ -96,3 +106,65 @@ async def query_events_crud(
     result = await db.execute(stmt)
     return result.scalars().all()
 
+
+async def get_track_by_track_id(db: AsyncSession, track_id: str) -> Track | None:
+    result = await db.execute(select(Track).where(Track.track_id == track_id))
+    return result.scalar_one_or_none()
+
+
+async def get_track_by_name(db: AsyncSession, name: str) -> Event | None:
+    result = await db.execute(select(Track).where(Track.name == name))
+    return result.scalar_one_or_none()
+
+
+async def create_track_crud(db: AsyncSession, track: Track) -> Track:
+    db.add(track)
+    await db.commit()
+    await db.refresh(track)
+    # 显式加载 region 和 season
+    result = await db.execute(
+        select(Track)
+        .options(selectinload(Track.event))
+        .where(Track.id == track.id)
+    )
+    return result.scalar_one()
+
+
+async def update_track_crud(db: AsyncSession, track: Track, update_data: dict):
+    for field, value in update_data.items():
+        setattr(track, field, value)
+    db.add(track)
+    await db.commit()
+    await db.refresh(track)
+
+
+async def query_tracks_crud(
+    db: AsyncSession,
+    track_name: Optional[str],
+    event_name: Optional[str],
+    season_name: Optional[str],
+    region_name: Optional[str],
+    sport_type: Optional[str],
+    page: int,
+    size: int
+) -> List[Track]:
+    stmt = select(Track).options(
+        selectinload(Track.event).selectinload(Event.season),
+        selectinload(Track.event).selectinload(Event.region)
+    ).join(Track.event).join(Event.season).join(Event.region)
+
+    if event_name:
+        stmt = stmt.filter(func.lower(Event.name).contains(event_name.lower()))
+    if season_name:
+        stmt = stmt.filter(func.lower(Season.name).contains(season_name.lower()))
+    if region_name:
+        stmt = stmt.filter(func.lower(Region.name).contains(region_name.lower()))
+    if sport_type:
+        stmt = stmt.filter(func.lower(Season.sport_type).contains(sport_type.lower()))
+    if track_name:
+        stmt = stmt.filter(func.lower(Track.name).contains(track_name.lower()))
+
+    stmt = stmt.order_by(Track.created_at.asc()).offset((page - 1) * size).limit(size)
+
+    result = await db.execute(stmt)
+    return result.scalars().all()
--- app/services/competition.py
+++ app/services/competition.py
@@ -1,20 +1,27 @@
 from app.crud.competition import (
     create_season_crud,
-    get_event_by_event_id,
+    get_event_by_event_id, get_event_by_name,
+    get_track_by_name, get_track_by_track_id,
     get_season_by_name_and_sport_type,
     get_season_by_season_id,
     update_season_crud,
     get_region_by_name,
-    create_event_crud,
-    update_event_crud,
-    query_events_crud,
+    create_event_crud, create_track_crud,
+    update_event_crud, update_track_crud,
+    query_events_crud, query_tracks_crud,
     create_region_crud
 )
 from app.core.errors import ErrorCode
 from app.schemas.base import BizException
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.schemas.competition import SeasonCreateForm, EventCreateForm, EventBaseInfo, EventUpdateForm, RegionCreate, SeasonBaseInfo
-from app.db.models import Season, Event, Region
+from app.schemas.competition import (
+    SeasonCreateForm, EventCreateForm,
+    EventBaseInfo, EventUpdateForm,
+    RegionCreate, SeasonBaseInfo,
+    TrackBaseInfo, TrackCreateForm,
+    TrackUpdateForm
+)
+from app.db.models import Season, Event, Region, Track
 from typing import Optional, List
 import uuid
 
@@ -63,22 +70,26 @@ async def create_region_service(db: AsyncSession, region_create: RegionCreate):
     await create_region_crud(db, new_region)
 
 
-async def create_event_service(db: AsyncSession, event: EventCreateForm, image_url: str) -> EventBaseInfo:
-    region = await get_region_by_name(db, event.region_name)
+async def create_event_service(db: AsyncSession, event_form: EventCreateForm, image_url: str) -> EventBaseInfo:
+    region = await get_region_by_name(db, event_form.region_name)
     if region is None:
         raise BizException(code=ErrorCode.REGION_NOT_FOUND, message="地理区域不存在")
 
-    season = await get_season_by_name_and_sport_type(db, event.season_name, event.sport_type.value)
+    season = await get_season_by_name_and_sport_type(db, event_form.season_name, event_form.sport_type.value)
     if season is None:
         raise BizException(code=ErrorCode.SEASON_NOT_FOUND, message="赛季不存在")
+    
+    event = await get_event_by_name(db, event_form.name)
+    if event is not None:
+        raise BizException(code=ErrorCode.EVENT_ALREADY_EXIST, message="赛事已存在，不可重复创建")
 
     event_id = f"event_{str(uuid.uuid4())[:8]}"
     new_event = Event(
         event_id=event_id,
-        name=event.name,
-        description=event.description,
-        start_date=event.start_date,
-        end_date=event.end_date,
+        name=event_form.name,
+        description=event_form.description,
+        start_date=event_form.start_date,
+        end_date=event_form.end_date,
         region_id=region.id,
         season_id=season.id,
         image_url=image_url
@@ -90,8 +101,8 @@ async def create_event_service(db: AsyncSession, event: EventCreateForm, image_u
         description=res.description,
         start_date=res.start_date.isoformat(),
         end_date=res.end_date.isoformat(),
-        region_name=res.region.name,
         season_name=res.season.name,
+        region_name=res.region.name,
         sport_type=res.season.sport_type,
         image_url=res.image_url
     )
@@ -150,3 +161,131 @@ async def query_events_service(
         sport_type=e.season.sport_type,
         image_url=e.image_url
     ) for e in events]
+
+
+async def create_track_service(db: AsyncSession, track_form: TrackCreateForm, image_url: str) -> TrackBaseInfo:
+    event = await get_event_by_name(db, track_form.event_name)
+    if event is None:
+        raise BizException(code=ErrorCode.EVENT_NOT_FOUND, message="赛事不存在")
+
+    region = await get_region_by_name(db, track_form.region_name)
+    if region is None:
+        raise BizException(code=ErrorCode.REGION_NOT_FOUND, message="地理区域不存在")
+
+    season = await get_season_by_name_and_sport_type(db, track_form.season_name, track_form.sport_type.value)
+    if season is None:
+        raise BizException(code=ErrorCode.SEASON_NOT_FOUND, message="赛季不存在")
+    
+    track = await get_track_by_name(db, track_form.name)
+    if track is not None:
+        raise BizException(code=ErrorCode.TRACK_ALREADY_EXIST, message="赛道已存在，不可重建创建")
+
+    track_id = f"track_{str(uuid.uuid4())[:8]}"
+    new_track = Track(
+        track_id = track_id,
+        name = track_form.name,
+        start_date = track_form.start_date,
+        end_date = track_form.end_date,
+        event_id = event.id,
+        from_lat = track_form.from_latitude,
+        from_lng = track_form.from_longitude,
+        to_lat = track_form.to_latitude,
+        to_lng = track_form.to_longitude,
+        elevation_difference = track_form.elevationDifference,
+        sub_region_name = track_form.subRegioName,
+        fee = track_form.fee,
+        prize_pool = track_form.prizePool,
+        image_url = image_url
+    )
+    res = await create_track_crud(db, new_track)
+    return TrackBaseInfo(
+        track_id=res.track_id,
+        name=res.name,
+        start_date=res.start_date.isoformat(),
+        end_date=res.end_date.isoformat(),
+        event_name=res.event.name,
+        season_name=res.event.season.name,
+        region_name=res.event.region.name,
+        sport_type=res.event.season.sport_type,
+        image_url=res.image_url,
+        from_latitude=res.from_lat,
+        from_longitude=res.from_lng,
+        to_latitude=res.to_lat,
+        to_longitude=res.to_lng,
+        elevation_difference=res.elevation_difference,
+        sub_region_name=res.sub_region_name,
+        fee=res.fee,
+        prize_pool=res.prize_pool
+    )
+
+
+async def update_track_service(db: AsyncSession, track: TrackUpdateForm, image_url: str):
+    existing_track = await get_track_by_track_id(db, track.track_id)
+    if existing_track is None:
+        raise BizException(code=ErrorCode.TRACK_NOT_FOUND, message="赛道不存在")
+    update_data = {
+        "name": track.name,
+        "start_date": track.start_date,
+        "end_date": track.end_date,
+        "from_lat": track.from_latitude,
+        "from_lng": track.from_longitude,
+        "to_lat": track.to_latitude,
+        "to_lng": track.to_longitude,
+        "elevationDifference": track.elevationDifference,
+        "subRegioName": track.subRegioName,
+        "fee": track.fee,
+        "prizePool": track.prizePool,
+        "image_url": image_url
+    }
+    await update_track_crud(db, existing_track, update_data)
+
+
+async def update_track_image_url(db: AsyncSession, track_id: str, image_url: str):
+    existing_track = await get_track_by_track_id(db, track_id)
+    if existing_track is None:
+        raise BizException(code=ErrorCode.TRACK_NOT_FOUND, message="赛道不存在")
+    update_data = {
+        "image_url": image_url
+    }
+    await update_track_crud(db, existing_track, update_data)
+
+
+async def query_tracks_service(
+    db: AsyncSession,
+    track_name: Optional[str],
+    event_name: Optional[str],
+    season_name: Optional[str],
+    region_name: Optional[str],
+    sport_type: Optional[str],
+    page: int,
+    size: int
+) -> List[TrackBaseInfo]:
+    tracks = await query_tracks_crud(
+        db=db,
+        track_name=track_name,
+        event_name=event_name,
+        season_name=season_name,
+        region_name=region_name,
+        sport_type=sport_type,
+        page=page,
+        size=size
+    )
+    return [TrackBaseInfo(
+        track_id=t.track_id,
+        name=t.name,
+        start_date=t.start_date.isoformat(),
+        end_date=t.end_date.isoformat(),
+        event_name=t.event.name,
+        season_name=t.event.season.name,
+        region_name=t.event.region.name,
+        sport_type=t.event.season.sport_type,
+        image_url=t.image_url,
+        from_latitude=str(t.from_lat),
+        from_longitude=str(t.from_lng),
+        to_latitude=str(t.to_lat),
+        to_longitude=str(t.to_lng),
+        elevation_difference=str(t.elevation_difference),
+        sub_region_name=t.sub_region_name,
+        fee=str(t.fee),
+        prize_pool=str(t.prize_pool)
+    ) for t in tracks]
